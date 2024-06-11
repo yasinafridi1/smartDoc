6709a2ac589fc8f244ab7c474748dbe5c5e96561
yasinafridi1 <yaseenafridi10875@gmail.com>
2024-06-11 15:16:20 +0500

    updated code

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import "./index.css";
 import App from "./App";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ToastContainer />
       <App />
     </Provider>
   </React.StrictMode>
